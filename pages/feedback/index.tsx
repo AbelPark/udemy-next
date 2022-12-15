a22319606362956d5886ac91f85f5524a7282394
AbelPark <56255317+AbelPark@users.noreply.github.com>
2022-12-15 11:00:51 +0900

    동적 API 라우트 추가

--- pages/feedback/index.tsx
+++ pages/feedback/index.tsx
@@ -1,21 +1,36 @@
-import { buildFeedbackPath, extractFeedback } from "../api/feedback.js";
+import axios from "axios";
+import { useState } from "react";
+import { buildFeedbackPath, extractFeedback } from "../api/feedback";
 
 export default function FeedbackPage(props: any) {
-  console.log(props.feedbackItems);
+  const [feedbackData, setFeedbackData]: any = useState();
+  async function loadFeedbackHandler(id: string) {
+    const { data } = await axios.get(`/api/feedback/${id}`); // /api/some-feedback-id
+    setFeedbackData(data.feedback);
+  }
 
   return (
-    <ul>
-      {props.feedbackItems.map((item: any) => {
-        return <li key={item.id}>{item.email}</li>;
-      })}
-    </ul>
+    <>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item: any) => {
+          return (
+            <li key={item.id}>
+              {item.email}
+              <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+                show Detail
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 }
 
 export async function getStaticProps() {
   const filePath = buildFeedbackPath();
   const data = extractFeedback(filePath);
-  console.log(data);
 
   return {
     props: {
